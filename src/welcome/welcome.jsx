9c3731599fa10e7c079413334714db11086c2e6c
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-26 19:32:19 +0100

    reload fixed

--- src/welcome/welcome.jsx
+++ src/welcome/welcome.jsx
@@ -3,7 +3,7 @@ import Buttons from "../tools_side/lang-panel";
 import { Link } from "react-router-dom";
 import App_controle_UpPanel from "../tools_side/app_controle_UpPanel";
 
-export default function welcome({props}) {
+export default function welcome({ props }) {
   function go_to_lp_dashbord() {
     history.push("/lp/dashbord");
   }
@@ -14,6 +14,7 @@ export default function welcome({props}) {
       </div>
       <br />
       <br />
+      <br />
       <main>
         <div className="welcome_text">
           <p>
@@ -21,20 +22,22 @@ export default function welcome({props}) {
             digital wonders.
             <br />
             <br />
-            Ready to embark on a journey through my projects? Click "Start"
-            below to discover captivating front-end designs, robust back-end
-            systems, and the seamless blend of technology and creativity.
-            <br />
+            🚀 I'm a highly skilled developer over both front-end and back-end
+            technologies. My versatility, agility, and deep respect for coding
+            principles define my approach.
+              🌟 I'm not just proficient; I'm a polymath, ready to tackle any
+              application, from streaming apps to complex systems.
             
+            <br />
             <p className="addetional_para">
-
               Curious to learn more about me? Just tap that "Start" button and
               let's explore together!
             </p>
           </p>
         </div>
+
         <div className="welcome_side_button">
-          <button className="btn">
+          <Link to={"/"} className="btn">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="40"
@@ -46,24 +49,13 @@ export default function welcome({props}) {
               <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
             </svg>
             <h2>Share</h2>
-          </button>
+          </Link>
           <br />
           <br />
           <br />
-          <button className="btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="40"
-              height="40"
-              fill="currentColor"
-              className="bi bi-file-earmark-person"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-              <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2v9.255S12 12 8 12s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h5.5v2z" />
-            </svg>
-            <h2>CV</h2>
-          </button>
+          <Link to={"/lp/dashbord"} className="btn">
+            <h2>START</h2>
+          </Link>
         </div>
         {/* ---------------------------------phone side buttons-------------------------------- */}
         <div className="welcome_phone_side_button">
@@ -81,7 +73,7 @@ export default function welcome({props}) {
             <h2>Share</h2>
           </Link>
 
-          <Link to={'/'} className="btn">
+          <Link to={"/"} className="btn">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -100,74 +92,24 @@ export default function welcome({props}) {
 
       {/* news bar ----------------------------- */}
 
-      <div className="news_bar">
-        <Link to={'/lp/dashbord'} className="btn" >
+      {/* <div className="news_bar">
+        <Link to={"/lp/dashbord"} className="btn">
           <h2>START</h2>
         </Link>
 
         <p className="news_bar_text">
-          Communication: Bridging code and understanding. Communication:
-          Bridging code and understanding. Communication: Bridging code and
-          understanding.
+          Communication: Bridging code and understanding.
         </p>
-      </div>
+      </div> */}
 
       {/* -------------------------------------------phone actions---------------------------- */}
 
       <div className="phone_actions">
-        <Link to={'ph/dashbord'} className="btn">
+        <Link to={"ph/dashbord"} className="btn">
           <h1>START</h1>
         </Link>
-        {/* <p>Contact me:</p> */}
-        {/* <div className="contacts_icons"> */}
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="currentColor"
-            className="bi bi-whatsapp"
-            viewBox="0 0 16 16"
-          >
-            <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.1h52.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
-          </svg> */}
-
-          {/* ------------------------ */}
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="currentColor"
-            className="bi bi-linkedin"
-            viewBox="0 0 16 16"
-          >
-            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
-          </svg> */}
-          {/* ------------------------ */}
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="currentColor"
-            className="bi bi-github"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-          </svg> */}
-          {/* ---------------------------- */}
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="currentColor"
-            className="bi bi-envelope"
-            viewBox="0 0 16 16"
-          >
-            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-          </svg> */}
-        {/* </div> */}
       </div>
       <App_controle_UpPanel />
-
     </div>
   );
 }
